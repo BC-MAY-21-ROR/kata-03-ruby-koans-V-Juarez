3e8e3299e14ebfd1e92b52243b85a6723e2eef97
Victoriano <mytacam78@gmail.com>
2022-06-10 16:30:26 -0600

    build(work) Excellent. 151 completed.

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  :equilateral if a == b && b == c && a == c
+  :isosceles if (a == c) || (a == c) || (b == c)
+  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
