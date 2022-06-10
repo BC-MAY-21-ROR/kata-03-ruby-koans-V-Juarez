d6b1c5e6ced340e02127ef5e89a93ebcdac9d251
Victoriano <mytacam78@gmail.com>
2022-06-10 17:33:42 -0600

    build(work) 191 completed.

--- koans/about_scoring_project.rb
+++ koans/about_scoring_project.rb
@@ -31,6 +31,33 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 def score(dice)
   # You need to write this method
+  d = {}
+	dice.each do |i|
+		d[i] = 0 if d[i].nil?
+		d[i] += 1
+	end
+	n = 0
+	d.each do |k,v|
+		if v >= 3
+			if k == 1
+				n += 1000
+			else
+				n += 100*k
+			end
+			v -= 3
+		end
+		v.times do
+			n += case k
+				when 5
+					50
+				when 1
+					100
+				else
+					0
+				end
+		end
+	end
+	n
 end
 
 class AboutScoringProject < Neo::Koan
